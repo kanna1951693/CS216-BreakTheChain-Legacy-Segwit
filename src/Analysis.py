81105a4663dcbf73bf935d74d927955de7494f14
Kunal1951 <kanna1951693@gmail.com>
2026-03-09 17:12:28 +0530

    Implement legacy and segwit address funding and analysis
    Added functionality to create and fund legacy and segwit addresses, and analyze transaction sizes and weights.

--- src/Analysis.py
+++ src/Analysis.py
@@ -7,18 +7,56 @@ try:
 except:
     pass
 
-legacy_ab = "511d7869d9511f50a354e85065da220b317a057f717604a903dcc0edb13b1e56"
-legacy_bc = "97749925507614bc96f4ef971e794d79cdcf39eb876cee64b502c272ea6a35cd"
-segwit_ab = "ed1a3eef7bdca712d15db2d7f193e93c580edf8c72ff4fc5ab11f6548a2fc8d5"
-segwit_bc = "f40de80868cd54e5294786dbfef095255f7ea055364d25b83d8db7c257bb8c2d"
+miner = rpc.getnewaddress()
+rpc.generatetoaddress(101, miner)
 
-def get_info(txid):
-    return rpc.decoderawtransaction(rpc.getrawtransaction(txid))
+# legacy addresses
+A = rpc.getnewaddress("", "legacy")
+B = rpc.getnewaddress("", "legacy")
+C = rpc.getnewaddress("", "legacy")
+rpc.sendtoaddress(A, 1)
+rpc.generatetoaddress(1, miner)
 
-t1 = get_info(legacy_ab)
-t2 = get_info(legacy_bc)
-t3 = get_info(segwit_ab)
-t4 = get_info(segwit_bc)
+utxo_A = next(u for u in rpc.listunspent() if u["address"] == A)
+funded_AB = rpc.fundrawtransaction(rpc.createrawtransaction([{"txid": utxo_A["txid"], "vout": utxo_A["vout"]}], {B: 0.5}), {"changeAddress": A})
+signed_AB = rpc.signrawtransactionwithwallet(funded_AB["hex"])
+legacy_ab_tx = rpc.decoderawtransaction(funded_AB["hex"])
+txid_AB = rpc.sendrawtransaction(signed_AB["hex"])
+rpc.generatetoaddress(1, miner)
+
+utxo_B = next(u for u in rpc.listunspent() if u["address"] == B)
+funded_BC = rpc.fundrawtransaction(rpc.createrawtransaction([{"txid": utxo_B["txid"], "vout": utxo_B["vout"]}], {C: 0.25}), {"changeAddress": B})
+signed_BC = rpc.signrawtransactionwithwallet(funded_BC["hex"])
+legacy_bc_tx = rpc.decoderawtransaction(funded_BC["hex"])
+txid_BC = rpc.sendrawtransaction(signed_BC["hex"])
+rpc.generatetoaddress(1, miner)
+
+# segwit addresses
+A2 = rpc.getnewaddress("", "p2sh-segwit")
+B2 = rpc.getnewaddress("", "p2sh-segwit")
+C2 = rpc.getnewaddress("", "p2sh-segwit")
+rpc.sendtoaddress(A2, 1)
+rpc.generatetoaddress(1, miner)
+
+utxo_A2 = next(u for u in rpc.listunspent() if u["address"] == A2)
+funded_AB2 = rpc.fundrawtransaction(rpc.createrawtransaction([{"txid": utxo_A2["txid"], "vout": utxo_A2["vout"]}], {B2: 0.5}), {"changeAddress": A2})
+signed_AB2 = rpc.signrawtransactionwithwallet(funded_AB2["hex"])
+segwit_ab_tx = rpc.decoderawtransaction(funded_AB2["hex"])
+txid_AB2 = rpc.sendrawtransaction(signed_AB2["hex"])
+rpc.generatetoaddress(1, miner)
+
+utxo_B2 = next(u for u in rpc.listunspent() if u["address"] == B2)
+funded_BC2 = rpc.fundrawtransaction(rpc.createrawtransaction([{"txid": utxo_B2["txid"], "vout": utxo_B2["vout"]}], {C2: 0.25}), {"changeAddress": B2})
+signed_BC2 = rpc.signrawtransactionwithwallet(funded_BC2["hex"])
+segwit_bc_tx = rpc.decoderawtransaction(funded_BC2["hex"])
+txid_BC2 = rpc.sendrawtransaction(signed_BC2["hex"])
+rpc.generatetoaddress(1, miner)
+
+# analysis
+t1 = legacy_ab_tx
+t2 = legacy_bc_tx
+t3 = segwit_ab_tx
+t4 = segwit_bc_tx
 
 print("legacy a->b:", t1["size"], "bytes,", t1["vsize"], "vsize,", t1["weight"], "weight")
 print("legacy b->c:", t2["size"], "bytes,", t2["vsize"], "vsize,", t2["weight"], "weight")
